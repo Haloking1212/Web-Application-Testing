6e4bbaca19af2950d6c162bec17b589b0a7c53c0
Hector Valdez <hvaldez180@gmail.com>
2019-10-31 16:45:59 -0400

    debugging scoreBoard

---
+++ baseball/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import ScoreBoard from "./components/ScoreBoard"
+
+function App() {
+  return (
+    <div className="App">
+      <ScoreBoard />
+    </div>
+  );
+}
+
+export default App;
